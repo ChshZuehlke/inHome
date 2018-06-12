c93a0bca27f5eb4d810446cecb6f1a5ef4a8c291
chsc <christian.gauch@zuehlke.com>
2018-06-12 10:45:27 +0200

    - Fixed wrong main method (It nows starts our app and not the sample one)

--- lib/widgets/RoomWidget.dart
+++ lib/widgets/RoomWidget.dart
@@ -17,6 +17,9 @@ class RoomState extends State<RoomWidget> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+        appBar: new AppBar(
+          title: const Text('InHome'),
+        ),
       body: new Center(
         child: new CustomPaint(
           size: new Size(200.0, 100.0),
